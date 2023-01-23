0f4cff28001216a463421d6debd5de5316b57d37
PalashJauhari <43773070+PalashJauhari@users.noreply.github.com>
2023-01-23 17:32:11 +0530

    Update health.py

--- health.py
+++ health.py
@@ -35,7 +35,7 @@ def healthLayout(DataReader,inputDict):
 
     if inputDict["value_all_states_metric_dropdown"] in ['women_overweight',"women_high_bp", 'women_high_sugar',
                                                          'men_overweight','men_high_bp','men_high_sugar',"children_wasted",
-                                                         "imr",'fertility_rate','sex_ratio']:
+                                                         "imr",'fertility_rate']:
 
         var1 = inputDict["value_all_states_metric_dropdown"] 
         df = df_health_status
@@ -52,6 +52,24 @@ def healthLayout(DataReader,inputDict):
                           margin=dict(l=0, r=0, t=25, b=0),height=1000)
         fig.add_vline(x=np.mean(df[var1]), line_width=3, line_dash="dash", line_color="black")
         fig.update_layout(margin=dict(l=0, r=0, t=25, b=0),height=1000)
+        
+    if inputDict["value_all_states_metric_dropdown"] in ['sex_ratio']:
+
+        var1 = inputDict["value_all_states_metric_dropdown"] 
+        df = df_health_status
+        df = df[df["Residence_Type"]=="Total"]
+        df = df.sort_values(var1,ascending=True)
+        df = df.reset_index().drop(columns="index")
+        
+        x1 = list(np.abs(df[var1].values))
+        y1 = list(df["State"].values)
+        text1 = [str(np.round(i,1)) for i in x1]
+        fig = go.Figure(go.Bar(x=x1,y=y1,orientation='h',name=var1,text=text1,textposition='inside',
+                               marker=dict(color=x1,colorscale='turbo_r')))
+        fig.update_layout(title={"text":"<b>{} : {}</b>".format(var1,np.round(np.mean(df[var1]),1))},
+                          margin=dict(l=0, r=0, t=25, b=0),height=1000)
+        fig.add_vline(x=np.mean(df[var1]), line_width=3, line_dash="dash", line_color="black")
+        fig.update_layout(margin=dict(l=0, r=0, t=25, b=0),height=1000)
 
     if inputDict["value_all_states_metric_dropdown"] in ["fully_immunised"]:
 
@@ -65,7 +83,7 @@ def healthLayout(DataReader,inputDict):
         y1 = list(df["State"].values)
         text1 = [str(np.round(i,1))+" %" for i in x1]
         fig = go.Figure(go.Bar(x=x1,y=y1,orientation='h',name=var1,text=text1,textposition='inside',
-                               marker=dict(color=x1,colorscale='turbo')))
+                               marker=dict(color=x1,colorscale='turbo_r')))
         fig.update_layout(title={"text":"<b>{} : {} %</b>".format(var1,np.round(np.mean(df[var1]),1))},
                           margin=dict(l=0, r=0, t=25, b=0),height=1000)
         fig.add_vline(x=np.mean(df[var1]), line_width=3, line_dash="dash", line_color="black")
@@ -82,7 +100,7 @@ def healthLayout(DataReader,inputDict):
         y1 = list(df["State"].values)
         text1 = [str(np.round(i*1e3,3)) for i in x1]
         fig = go.Figure(go.Bar(x=x1,y=y1,orientation='h',name=var1,text=text1,textposition='inside',
-                               marker=dict(color=x1,colorscale='turbo')))
+                               marker=dict(color=x1,colorscale='turbo_r')))
         fig.update_layout(title={"text":"<b>{} Per Thousand Population : {} </b>".format(var1,np.round(1e3*np.mean(x1),3))},
                           margin=dict(l=0, r=0, t=25, b=0),height=1000)
         fig.add_vline(x=np.mean(df[var1]), line_width=3, line_dash="dash", line_color="black")
